864f79c98b387bd3d9be9130282259cb798fcf76
jungjing1221 <73683393+jungjing1221@users.noreply.github.com>
2020-11-18 01:50:19 +0700

    Update editupdate.php

--- editupdate.php
+++ editupdate.php
@@ -17,8 +17,6 @@ $query = mysqli_query($conn,$sql);
 
 if ($conn->query($sql) === TRUE) {
     header('Location:show.php');
-  } else {
-    echo "Error updating record: " . $conn->error;
   }
 mysqli_close($conn);
 ?>
